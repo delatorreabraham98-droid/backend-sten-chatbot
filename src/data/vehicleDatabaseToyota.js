76ed06610165c427be1ed53037a18d9fe3094860
delatorreabraham98-droid <delatorreabraham98@gmail.com>
2026-05-21 20:11:38 -0700

    Create vehicleDatabaseToyota.js

---
+++ src/data/vehicleDatabaseToyota.js
@@ -0,0 +1,51 @@
+export const TOYOTA_DATABASE = {
+
+  rav4: {
+    lowBeam: '9006',
+    highBeam: '9005',
+    fog: 'H11',
+    type: 'split',
+    aliases: ['rav4', 'rav 4', 'rav']
+  },
+
+  camry: {
+    lowBeam: 'H11',
+    highBeam: '9005',
+    fog: 'H11',
+    type: 'split',
+    aliases: ['camry', 'camri']
+  },
+
+  corolla: {
+    lowBeam: '9006',
+    highBeam: '9005',
+    fog: 'H11',
+    type: 'split',
+    aliases: ['corolla']
+  },
+
+  tacoma: {
+    lowBeam: 'H11',
+    highBeam: '9005',
+    fog: 'H11',
+    type: 'split',
+    aliases: ['tacoma']
+  },
+
+  tundra: {
+    lowBeam: '9006',
+    highBeam: '9005',
+    fog: 'H11',
+    type: 'split',
+    aliases: ['tundra']
+  },
+
+  hilux: {
+    lowBeam: 'H4',
+    highBeam: 'H4',
+    fog: 'H11',
+    type: 'dual',
+    aliases: ['hilux']
+  }
+
+};
